41cbba6f3c20e1e74500a2ad9b875ccce89b74e5
obre-bjorn <obrebjorn@gmail.com>
2021-03-05 23:23:08 +0300

    Return a string representation of the Product model to the Admin Site

--- product/models.py
+++ product/models.py
@@ -9,3 +9,9 @@ class Product(models.Model):
     date_added  = models.DateTimeField(auto_now_add = True)
     weight      = models.FloatField(max_length = 10)
     price       = models.FloatField()
+
+
+
+    def __str__(self):
+        return self.name
+    
