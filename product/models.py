12b7ab068ccc14cb6d21bb789e9cc4306f66ba3d
obre-bjorn <obrebjorn@gmail.com>
2021-03-05 22:44:51 +0300

    Adding product properties

--- product/models.py
+++ product/models.py
@@ -3,5 +3,9 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     """ Product to be sold """
-    name    = models.CharField(max_length = 200)
-    price  
+    name        = models.CharField(max_length = 200)
+    date_added  = models.DateTimeField(auto_now_add = True)
+    weight      = models.FloatField(max_length =)
+    price       = models.FloatField
+    category    =
+    image       = models.ImageField(_(""), upload_to=None, height_field=None, width_field=None, max_length=None) 
