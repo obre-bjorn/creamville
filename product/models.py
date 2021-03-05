d03cf9369ddfbeaaa407380ce921b6aaba34669d
obre-bjorn <obrebjorn@gmail.com>
2021-03-05 22:51:24 +0300

    Registered product as third party app

--- product/models.py
+++ product/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Product(models.Model):
     """ Product to be sold """
     name        = models.CharField(max_length = 200)
     date_added  = models.DateTimeField(auto_now_add = True)
-    weight      = models.FloatField(max_length =)
-    price       = models.FloatField
-    category    =
-    image       = models.ImageField(_(""), upload_to=None, height_field=None, width_field=None, max_length=None) 
+    weight      = models.FloatField(max_length = 10)
+    price       = models.FloatField()
