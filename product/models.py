9f0df5802da7add5126bda9434b0a95fb1c1231f
obre-bjorn <obrebjorn@gmail.com>
2021-03-09 00:21:34 +0300

    Added category choices to product model

--- product/models.py
+++ product/models.py
@@ -2,16 +2,35 @@ from django.db import models
 
 # Create your models here.
 
+CATEGORY_CHOICE = ('Cakes','Cookies','Pastries')
+
 
 class Product(models.Model):
+
+    CAKE        = 1
+    SNACK       = 2
+    PASTRY      = 3
+
+    P_CHOICES   = (
+        (CAKE, ('Cakes')),
+        (SNACK, ('Snacks')),
+        (PASTRY, ('Pastry')),
+    )
+
     """ Product to be sold """
+    
     name        = models.CharField(max_length = 200)
     date_added  = models.DateTimeField(auto_now_add = True)
     weight      = models.FloatField(max_length = 10)
+    caption     = models.TextField()
+    category    = models.CharField(choices = P_CHOICES,default = CAKE, max_length = 10)
     price       = models.FloatField()
+    
+    
 
 
     """ Return a string representation of the model """
+    
     def __str__(self):
         return self.name
     
