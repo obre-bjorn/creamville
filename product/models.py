82cd5353135264c961d14ece596e30aba3993476
obre-bjorn <obrebjorn@gmail.com>
2021-03-09 01:32:58 +0300

    Added media root
    Added product image model and changed the category choices

--- product/models.py
+++ product/models.py
@@ -2,28 +2,25 @@ from django.db import models
 
 # Create your models here.
 
-CATEGORY_CHOICE = ('Cakes','Cookies','Pastries')
+CATEGORY_CHOICE = (
+    ('CA','Cakes'),
+    ('CO','Cookies'),
+    ('PA','Pastries')
+)
 
 
 class Product(models.Model):
 
-    CAKE        = 1
-    SNACK       = 2
-    PASTRY      = 3
-
-    P_CHOICES   = (
-        (CAKE, ('Cakes')),
-        (SNACK, ('Snacks')),
-        (PASTRY, ('Pastry')),
-    )
+  
 
     """ Product to be sold """
     
     name        = models.CharField(max_length = 200)
     date_added  = models.DateTimeField(auto_now_add = True)
     weight      = models.FloatField(max_length = 10)
+    p_image     = models.ImageField(upload_to = 'images/',null = True )
     caption     = models.TextField()
-    category    = models.CharField(choices = P_CHOICES,default = CAKE, max_length = 10)
+    category    = models.CharField(choices = CATEGORY_CHOICE,null = True, max_length = 10)
     price       = models.FloatField()
     
     
